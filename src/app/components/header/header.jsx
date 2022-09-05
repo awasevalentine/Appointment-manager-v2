50da1682fb48ea5da6e60fa2cd65805f247cb1f6
awasevalentine <awasevalentine@gmail.com>
2022-09-05 23:30:41 +0100

    created a navbar section for the navigation header

--- src/app/components/header/header.jsx
+++ src/app/components/header/header.jsx
@@ -13,7 +13,7 @@ const NavSection = () => {
             <span class="navbar-toggler-icon"></span>
           </button>
           <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
-            <a class="navbar-brand" href="#">Home</a>
+            <a class="navbar-brand" href="/">Home</a>
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
               <li class="nav-item">
                 <a class="nav-link active" aria-current="page" href="/">New Appointment</a>
