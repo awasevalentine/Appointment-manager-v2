a8220667e9bb9a44761a4cc00ff3f52393c8209a
awasevalentine <awasevalentine@gmail.com>
2022-09-05 23:08:57 +0100

    Created a folder for holding reusable components, pages and assests folder for storing static files

---
+++ src/app/components/header/header.jsx
@@ -0,0 +1,52 @@
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import './header.css'
+
+const NavSection = () => {
+
+    const [isLoggedIn, setIsLoggedIn ] = useState(false);
+
+    return ( 
+        <nav class="navbar navbar-expand-lg bg-dark" style={{height: '80px'}}>
+        <div class="container-fluid">
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+            <a class="navbar-brand" href="#">Home</a>
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="/">New Appointment</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="/">Notifications</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="/">About</a>
+              </li>
+            </ul>
+            <div className="auth-wrapper">
+                {
+                    isLoggedIn ? (
+                        <> Me</>
+                    ):
+                    (
+                        <>
+                        <span className='signin'>
+                            <Link to='/register' class="signup" >Sign Up</Link>
+                        </span>
+                        <span className='signin'>
+                            <Link to='/login' class="login">Login</Link>
+                        </span>
+                        </>
+                    )
+                }
+
+            </div>
+          </div>
+        </div>
+      </nav>
+     );
+}
+ 
+export default NavSection;
---
+++ src/app/pages/home/home-page.jsx
@@ -0,0 +1,19 @@
+import NavSection from "../../components/header/header"
+
+
+const HomePage =()=>{
+
+
+    return (
+        <>
+        <NavSection />
+        <div id="home-wrapper">
+            <section className="intro">
+
+            </section>
+        </div>
+        </>
+    )
+}
+
+export default HomePage
