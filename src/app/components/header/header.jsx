02abb0a61403308bfea41499b4df9f13db73a3f1
awasevalentine <awasevalentine@gmail.com>
2022-09-06 23:15:55 +0100

    Added responsiveness to the header

--- src/app/components/header/header.jsx
+++ src/app/components/header/header.jsx
@@ -9,11 +9,14 @@ const NavSection = () => {
     return ( 
         <nav class="navbar navbar-expand-lg bg-dark" style={{height: '80px'}}>
         <div class="container-fluid">
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+          <button class="navbar-toggler bg-#FAF3E3" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
+          <div className='brand'>
+            <a class="brand-name text-center" href="/">Home</a>
+          </div>
+
           <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
-            <a class="navbar-brand" href="/">Home</a>
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
               <li class="nav-item">
                 <a class="nav-link active" aria-current="page" href="/">New Appointment</a>
