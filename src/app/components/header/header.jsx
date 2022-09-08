072639d06c008ef90088e6f89f2e9cea70b46eb0
awasevalentine <awasevalentine@gmail.com>
2022-09-08 01:29:52 +0100

    Added the intro section for the landing page

--- src/app/components/header/header.jsx
+++ src/app/components/header/header.jsx
@@ -7,7 +7,7 @@ const NavSection = () => {
     const [isLoggedIn, setIsLoggedIn ] = useState(false);
 
     return ( 
-        <nav class="navbar navbar-expand-lg" style={{height: '80px'}}>
+        <nav class="navbar navbar-expand-lg sticky-top" style={{height: '80px'}}>
         <div class="container-fluid">
           <button class="navbar-toggler bg-#FAF3E3" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
--- src/app/pages/home/home-page.jsx
+++ src/app/pages/home/home-page.jsx
@@ -1,5 +1,7 @@
-import NavSection from "../../components/header/header"
-
+import { Link } from "react-router-dom";
+import NavSection from "../../components/header/header";
+import { BsArrowRightShort } from 'react-icons/bs'
+import './home.css'
 
 const HomePage =()=>{
 
@@ -9,7 +11,12 @@ const HomePage =()=>{
         <NavSection />
         <div id="home-wrapper">
             <section className="intro">
-
+            <h1 class="display-6">Appointment Manager</h1>
+            <p className="text-centert intro-text">
+                Get those appointments sorted out and well managed.
+                Set reminder, track their progress, and get notified.
+            </p>
+            <Link class="get-started" to='/'>Get Started <BsArrowRightShort class="fs-4 arrow-heartbeat" /></Link>
             </section>
         </div>
         </>
