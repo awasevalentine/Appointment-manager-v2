010ab7c4846bc335ca9eebfe71612d0397813457
awasevalentine <awasevalentine@gmail.com>
2022-09-10 20:50:26 +0100

    added a section for the appointment offer benefit

--- src/services/static-data.jsx
+++ src/services/static-data.jsx
@@ -17,3 +17,22 @@ export const benefits = [
         description: ''
     }
 ]
+
+export const benefitOffers  =[
+    {
+        name: 'Appointment',
+        description: 'Have that appointment booked,cancelled or reschedule with ease. And also schedule your daily task',
+        image: `${require('../assets/home_page/Create_appointment2.jpeg')}`
+    },
+
+    {
+        name: 'Monitor Status',
+        description: 'Monitor the progress, and status of each task or appointments date and time',
+        image: `${require('../assets/home_page/monitor.jpg')}`
+    },
+    {
+        name: 'Get Notify',
+        description: 'Get constant reminder of the appointment time, and also get notify if it status changes',
+        image: `${require('../assets/home_page/notification-207419.jpg')}`
+    },
+]
