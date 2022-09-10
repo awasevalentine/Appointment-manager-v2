d4114bed24a398af4bb0b7e2631120f768554009
awasevalentine <awasevalentine@gmail.com>
2022-09-10 11:41:10 +0100

    added images for calendar

---
+++ src/services/static-data.jsx
@@ -0,0 +1,19 @@
+
+
+export const benefits = [
+    {
+        name: 'New Appointment',
+        icon: 'RiCalendarTodoFill',
+        description: ''
+    },
+    {
+        name: 'Track Status',
+        icon: 'MdMonitor',
+        description: ''
+    },
+    {
+        name: 'Get Notified',
+        icon: 'MdCircleNotifications',
+        description: ''
+    }
+]
