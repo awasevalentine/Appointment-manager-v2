e62629a7fede5b624f2fcfbe245291af8c61fa45
awasevalentine <awasevalentine@gmail.com>
2022-10-11 23:21:49 +0100

    adjusted the the media query for extra small devices

--- src/app/pages/home/home-page.jsx
+++ src/app/pages/home/home-page.jsx
@@ -47,9 +47,14 @@ const HomePage =()=>{
                     <MdCircleNotifications className="fs-3" />
                 </span>
             </div>
-            <div className="content">
+            <div className="content d-none d-md-flex">
                 <span><p className="benefits-content">Create appointments or tasks</p></span>
                 <span><p className="benefits-content">Track status of task or Appointment</p></span>
+                <span><p className="benefits-content">Get notified</p></span>
+            </div>
+            <div className="benefits-mobile-content d-md-none">
+                <span><p className="benefits-content">Appointments</p></span>
+                <span><p className="benefits-content">Track status</p></span>
                 <span><p className="benefits-content">Get notified</p></span>
             </div>
             <div  className="calendar-image">
@@ -70,8 +75,8 @@ const HomePage =()=>{
                 <div className="row">
                     {
                         benefitOffers.map((res)=>(
-                            <div className="col-sm-12 col-md-4 col-lg-3">
-                                <div className="card h-100" style={{height: '100% !important'}}>
+                            <div className="col-sm-12 col-md-4 col-lg-3" style={{margin:'6px'}}>
+                                <div className="card h-100" style={{margin:'8px'}}>
                                     <img src={res.image} class="card-img-top" alt="Appointment" />
                                     <h4 className="card-title mt-3">{res.name}</h4>
                                     <p className="card-body">
