a3ad69076bb5faa454cddec73cf41a5427c78bc8
awasevalentine <awasevalentine@gmail.com>
2022-10-11 23:20:08 +0100

    Added media query that will target all device width and hard for extra small screen configured'

--- src/app/components/footer/footer.jsx
+++ src/app/components/footer/footer.jsx
@@ -6,9 +6,9 @@ const Footer = () => {
         <>
         <div className='footer-main-wrapper'>
             <div class="footer-content">
-                <p>&copy; 2022 Appointment Manager </p>
-                <div className='vertical_rule'></div>
                 <span>All Right Reserved 🥨</span>
+                <div className='vertical_rule'></div>
+                <p>&copy; 2022 Appointment Manager </p>
             </div>
         </div>
         </>
